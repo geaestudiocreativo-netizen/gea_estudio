bff91bcb13aefbd0152866360d67ddff4a191399
GeaEstudio <geaestudiocreativo@gmail.com>
2026-08-03 20:58:48 +0200

    bugfixing: vercel not deploying

---
+++ src/data/projects.ts
@@ -0,0 +1,8 @@
+export const projectRoutes = [
+  { slug: 'talleres-alonso', title: 'Talleres Alonso' },
+  { slug: 'g', title: '.G' },
+  { slug: 'gea-artesania', title: 'Gea Artesania' },
+  { slug: 'foc', title: 'Foc' },
+  { slug: 'margen', title: 'Margen' },
+  { slug: 'trop', title: 'Trop' }
+];
