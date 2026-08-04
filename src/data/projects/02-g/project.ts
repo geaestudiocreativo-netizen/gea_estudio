5717fd14b5e0ef85492564c5e1a67ca41bc59b31
GeaEstudio <geaestudiocreativo@gmail.com>
2026-08-04 16:52:53 +0200

    branding-a layout added and working well

--- src/data/projects/02-g/project.ts
+++ src/data/projects/02-g/project.ts
@@ -4,7 +4,7 @@ const project: ProjectFolderMetadata = {
 	slug: 'g',
 	number: '02',
 	title: '.G',
-	client: 'By Gabriel García',
+	client: '.G By Gabriel García',
 	location: 'Valencia, España',
 	year: 2026,
 	description:
